a4b59565fd5830efd3f85dc5b519400ad466ddf5
Aayush-Raut10 <aayushraut209@gmail.com>
2026-06-18 22:00:35 +0545

    Adding .env file

--- app/database.py
+++ app/database.py
@@ -1,8 +1,12 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
+from dotenv import load_dotenv
+import os
 
 
-DATABASE_URL = "mysql+pymysql://root:Mysqlroot[]@localhost:3306/testdb"
+load_dotenv()
+
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(DATABASE_URL)
 
